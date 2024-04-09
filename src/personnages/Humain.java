9cc4ba8d46a0559fe049237785ee77729fb3a985
Lucas Lefebvre <lfl5405a@ensfsi.onmicrosoft.com>
2024-04-09 08:41:08 +0200

    Les humains ont des connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,6 +4,9 @@ public class Humain {
 	private String nom;
 	private String boissonFav;
 	private int argent;
+	protected int nbConnaissance;
+	private static final int TAILLE_MEM = 30;
+	protected Humain[] memoire = new Humain[TAILLE_MEM];
 
 	public Humain(String nom, String boissonFav, int argent) {
 		this.nom = nom;
@@ -54,6 +57,39 @@ public class Humain {
 
 	protected void perdreArgent(int perte) {
 		argent -= perte;
+	}
+
+	private void memoriser(Humain autreHumain) {
+		if (nbConnaissance >= TAILLE_MEM) {
+			for (int i = 0; i < nbConnaissance - 1; i++) {
+				memoire[i] = memoire[i + 1];
+			}
+			nbConnaissance--;
+		}
+		memoire[nbConnaissance] = autreHumain;
+		nbConnaissance++;
+	}
+
+	private void repondre(Humain autreHumain) {
+		direBonjour();
+		memoriser(autreHumain);
+
+	}
+
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		direBonjour();
+		autreHumain.repondre(this);
+		memoriser(autreHumain);
+	}
+
+	public void listerConnaissance() {
+		StringBuilder s = new StringBuilder();
+		s.append("Je connais beaucoup de monde dont : ");
+		for (int i = 0; i < nbConnaissance; i++) {
+			s.append(memoire[i].getNom());
+			s.append(", ");
+		}
+		System.out.println(s.toString());
 	}
 
 }
